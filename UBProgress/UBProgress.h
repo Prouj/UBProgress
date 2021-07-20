e6b98576f4f7260324774a6a8f3f642ab832c6f6
Paulo Rubem <rubem.oliveira27@hotmail.com>
2021-07-20 10:47:01 -0300

    first commit

---
+++ UBProgress/UBProgress.h
@@ -0,0 +1,18 @@
+//
+//  UBProgress.h
+//  UBProgress
+//
+//  Created by Paulo Uchôa on 20/07/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UBProgress.
+FOUNDATION_EXPORT double UBProgressVersionNumber;
+
+//! Project version string for UBProgress.
+FOUNDATION_EXPORT const unsigned char UBProgressVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UBProgress/PublicHeader.h>
+
+
---
+++ UBProgress/UBProgress.m
@@ -0,0 +1,8 @@
+//
+//  UBProgress.m
+//  UBProgress
+//
+//  Created by Paulo Uchôa on 20/07/21.
+//
+
+#import <UBProgress.h>
