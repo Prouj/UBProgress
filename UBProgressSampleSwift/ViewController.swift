a374fd79a8664a41329bf4db8c9249c091bf181d
Paulo Rubem <rubem.oliveira27@hotmail.com>
2021-07-21 11:15:17 -0300

    creation of targets for swift and objC

---
+++ UBProgressSampleSwift/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  UBProgressSampleSwift
+//
+//  Created by Paulo Uchôa on 21/07/21.
+//
+
+import UIKit
+import UBProgress
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var progressBar: UBProgress!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
